fe5b93497dbb487018c11c54a81c5e25f28514dd
Muhammad Anas <anas.ee128@gmail.com>
2026-04-04 02:46:57 +0500

    updated animation

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -122,7 +122,7 @@ const Works = () => {
           variants={fadeIn("", "", 0.1, 1)}
           className='mt-3 text-secondary text-sm md:text-base max-w-3xl leading-[30px]'
         >
-           projects showcases my skills and experience through
+          Following projects showcases my skills and experience through
           real-world examples of my work. Each project is briefly described with
           links to code repositories and live demos in it. It reflects my
           ability to solve complex problems, work with different technologies,
